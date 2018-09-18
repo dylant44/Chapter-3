ad49dc7e8b10fad7aecbb2490a53885ff8af56d8
Dylan Tomlinson <dt303271@students.davenport.k12.ia.us>
2018-09-18 16:46:38 -0500

    debugging

---
+++ Chapter 3/src/youDoIt/DelgadosTacos.java
@@ -0,0 +1,38 @@
+package youDoIt;
+
+public class DelgadosTacos 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		double tacoPrice = 1.99;
+		double burritoPrice = 1.99;
+		double enchiladaPrice = 1.99;
+		double tostadaPrice = 1.99;
+		double fajitaPrice = 1.99;
+		double chipPrice = 1.99;
+		double cheesePrice = 1.99;
+		double guacPrice = 1.99;
+		double salsaPrice = 1.99;
+		double carnitaPrice = 1.99;
+
+		double numTacosOrdered;
+		double numburritosOrdered;
+		double numEnchiladasOrdered;
+		double numTostadasOrdered;
+		double numfajitasOrdered;
+		double numchipsOrdered;
+		double numcheeseOrdered;
+		double numguacOrdered;
+		double numsalsaOrdered;
+		double numcarnitasOrdered;
+		
+		
+		
+		
+		
+		
+	}
+
+}
